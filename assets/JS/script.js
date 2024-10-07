729fe46681ecd964030dffa05ca86890ac1d44af
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2024-10-07 13:25:52 +0530

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,41 @@
 let sub_btn = document.querySelector(".sub-btn");
-
-QRCodeGen = (data) =>{
-  var typeNumber = 6;
+let ui = document.querySelector("#placeHolder");
+QRCodeGen = (tn,data) =>{
+  var typeNumber = tn;
   var errorCorrectionLevel = "H";
   var qr = qrcode(typeNumber, errorCorrectionLevel);
   qr.addData(data);
   qr.make();
   document.getElementById("placeHolder").innerHTML = qr.createImgTag();
 }
-
 sub_btn.addEventListener("click", () => {
+
     let url = document.getElementsByClassName("input-data")[0].value;
     if (url != "") {
-      console.log(url);
-      QRCodeGen(url);
+      document.getElementById("placeHolder").innerHTML = null
+      if (url.length<=57) {
+        console.log(url.length);
+        QRCodeGen(6,url.length);
+        console.log("QR Code is generated Succefully!!! ")
+      }
+      else if(url.length<=113){
+        console.log(url.length);
+        QRCodeGen(12,url);
+        console.log("QR Code is generated Succefully!!! ")
+      }
+      else if(url.length<=226){
+        console.log(url.length);
+        QRCodeGen(24,url);
+        console.log("QR Code is generated Succefully!!! ")
+      }
+      else{
+        let p = document.createElement("p");
+        p.setAttribute("class","input-error")
+        p.innerHTML = "Invalid INput Size";
+        ui.append(p);
+      }
     } else {
       alert("Invalid Input! Please Type something");
     }
+    
 });
