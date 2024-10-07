7d206bd8ef17cecf261a4308afc6caeb3c3b853a
Jatin Rane <156644466+jatinrane14@users.noreply.github.com>
2024-10-07 10:50:45 +0530

    Add files via upload

---
+++ assets/JS/script.js
@@ -0,0 +1,23 @@
+let sub_btn = document.querySelector(".sub-btn");
+
+QRCodeGen = (data) =>{
+  var typeNumber = 5;
+  var errorCorrectionLevel = "H";
+  var qr = qrcode(typeNumber, errorCorrectionLevel);
+  qr.addData(data);
+  qr.make();
+  document.getElementById("placeHolder").innerHTML = qr.createImgTag();
+}
+
+sub_btn.addEventListener("click", () => {
+  setTimeout(() => {
+    let url = document.getElementsByClassName("input-data")[0].value;
+    if (url != "") {
+      console.log(url);
+      QRCodeGen(url);
+    } else {
+      alert("Invalid Input! Please Type something");
+    }
+    
+  },500);
+});
