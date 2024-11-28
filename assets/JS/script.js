7e95abd83481c8dca2672e15dbe594faf6bf04e3
Jatin Rane <jatinrane23@gmail.com>
2024-11-28 22:03:59 +0530

    Updated

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,40 +1,37 @@
 let sub_btn = document.querySelector(".sub-btn");
-let ui = document.querySelector("#placeHolder");
+let qrcon = document.querySelector("#placeHolder");
 QRCodeGen = (tn,data) =>{
   var typeNumber = tn;
   var errorCorrectionLevel = "H";
   var qr = qrcode(typeNumber, errorCorrectionLevel);
   qr.addData(data);
   qr.make();
-  document.getElementById("placeHolder").innerHTML = qr.createImgTag();
+  qrcon.innerHTML = qr.createImgTag();
 }
-sub_btn.addEventListener("click", () => {
-
+sub_btn.addEventListener("click", ()=>{
     let url = document.getElementsByClassName("input-data")[0].value;
     if (url != "") {
-      document.getElementById("placeHolder").innerHTML = null
+      document.getElementById("placeHolder").innerHTML = null;
       if (url.length<=57) {
-        console.log(url.length);
-        QRCodeGen(6,url.length);
-        console.log("QR Code is generated Succefully!!! ")
+        QRCodeGen(6,url);
+        console.log("QR Code is generated Succefully!!! ");
       }
       else if(url.length<=113){
-        console.log(url.length);
         QRCodeGen(12,url);
-        console.log("QR Code is generated Succefully!!! ")
+        console.log("QR Code is generated Succefully!!! ");
       }
       else if(url.length<=226){
-        console.log(url.length);
         QRCodeGen(24,url);
         console.log("QR Code is generated Succefully!!! ")
       }
       else{
         let p = document.createElement("p");
-        p.setAttribute("class","input-error")
+        p.setAttribute("class","input-error");
         p.innerHTML = "Invalid INput Size";
-        ui.append(p);
+        qrcon.append(p);
       }
-    } else {
+    } 
+    else {
       alert("Invalid Input! Please Type something");
     }
     
